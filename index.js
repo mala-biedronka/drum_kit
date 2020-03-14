3534043f40d15d014d3133a8b973a6f3917f5df8
Iryna Predborska <iryna.predborska@gmail.com>
2020-03-14 18:57:07 +0100

    add event listener, change text color

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-let buttons = document.querySelectorAll("button");
+// let buttons = document.querySelectorAll("button");
+// for (let i = 0; i < buttons.length; i++) {
+//     buttons[i].addEventListener("click", function () {
+//         alert("Hi there!");
+//     });
+// }
+
+let buttons = document.querySelectorAll(".drum");
 for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener("click", function () {
-        alert("Hi there!");
+        this.style.color = "white";
+
     });
 }
 
+// let audio = new Audio("sounds/tom-1.mp3");
+// audio.play();
